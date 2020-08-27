9cf11d0c78ddbf564c6b429a378902addaa9166c
allanchan <allanchan339@gmail.com>
2020-08-28 01:50:58 +0800

    ENH: Reset password and email reset instruction function created

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #remember to type 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'allanchan339@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'djangotestforallan@gmail.com'
+EMAIL_HOST_PASSWORD = 'allanfortestdjango'
